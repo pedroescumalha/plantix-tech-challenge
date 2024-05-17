ec8a78881b7b2bf2d9ba6ae7db4461b1d733c898
Pedro Escumalha <pedrosimoes93@gmail.com>
2024-05-17 17:36:38 +0200

    add fastify

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello world");
+import fastify from "fastify";
+
+const server = fastify();
+
+server.get("/ping", () => {
+    return "pong\n";
+});
+
+server.listen({ port: 8080 }, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+});
