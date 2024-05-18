c1469641200e0b83ee30d34f2c965b6f2723e74b
Pedro Escumalha <pedrosimoes93@gmail.com>
2024-05-18 16:19:07 +0200

    create db client

---
+++ src/repositories/dbClient.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+
+type InitDbOutput = {
+    disconnect: () => Promise<void>;
+}
+
+let dbClient: PrismaClient | undefined = undefined;
+
+export function init(): InitDbOutput {
+    const db = getInstance();
+
+    return {
+        disconnect: db.$disconnect,
+    };
+}
+
+export function getInstance(): PrismaClient {
+    if (!dbClient) {
+        dbClient = new PrismaClient();
+    }
+
+    return dbClient;
+}
