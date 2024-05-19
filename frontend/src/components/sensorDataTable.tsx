2ae63f4a8ad5593c20663bc57d037f3671cdb054
Pedro Escumalha <pedrosimoes93@gmail.com>
2024-05-19 16:16:52 +0200

    update table

--- frontend/src/components/sensorDataTable.tsx
+++ frontend/src/components/sensorDataTable.tsx
@@ -1,51 +1,41 @@
 import React from 'react'
 import { TableHead, TableRow, TableHeader, TableCell, TableBody, Table } from './cn/table'
+import { SensorDataInput } from '../lib/sensorsDataService'
 
 type SensorDataTableProps = {
-  data: Array<{
-    type: string;
-    value: string;
-    timestamp: string;
-  }>
+  data: SensorDataInput[]
 }
 
 export default function SensorDataTable(props: SensorDataTableProps) {
+  const buildTable = () => {
+    return props.data.map((d) => {
+      return (
+        <TableRow>
+          <TableCell>{d.externalId}</TableCell>
+          <TableCell>{d.type}</TableCell>
+          <TableCell>{d.latitude}</TableCell>
+          <TableCell>{d.longitude}</TableCell>
+          <TableCell>{`${d.value} ${d.unit}`}</TableCell>
+          <TableCell>{d.timestamp}</TableCell>
+        </TableRow>
+      ) 
+    })
+  }
   return (
     <div className="border rounded-lg overflow-hidden">
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead>Sensor Name</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Type</TableHead>
+            <TableHead>Latitude</TableHead>
+            <TableHead>Longitude</TableHead>
             <TableHead>Value</TableHead>
             <TableHead>Timestamp</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell>Temperature Sensor</TableCell>
-            <TableCell>25.3 °C</TableCell>
-            <TableCell>2023-05-19 12:34:56</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Humidity Sensor</TableCell>
-            <TableCell>65%</TableCell>
-            <TableCell>2023-05-19 12:35:01</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Motion Sensor</TableCell>
-            <TableCell>Active</TableCell>
-            <TableCell>2023-05-19 12:35:10</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Light Sensor</TableCell>
-            <TableCell>500 lux</TableCell>
-            <TableCell>2023-05-19 12:35:15</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Pressure Sensor</TableCell>
-            <TableCell>1020 hPa</TableCell>
-            <TableCell>2023-05-19 12:35:20</TableCell>
-          </TableRow>
+          {buildTable()}
         </TableBody>
       </Table>
     </div>
