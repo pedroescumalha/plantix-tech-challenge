a590ec1edb725fe91d578a695b61360a52d57a17
Pedro Escumalha <pedrosimoes93@gmail.com>
2024-05-19 14:47:12 +0200

    add paginator

---
+++ frontend/src/components/paginator.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Pagination, PaginationContent, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from './cn/pagination'
+
+export function Paginator() {
+  return (
+    <div className="flex justify-end p-4">
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">2</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">3</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href="#" />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </div>
+  )
+}
