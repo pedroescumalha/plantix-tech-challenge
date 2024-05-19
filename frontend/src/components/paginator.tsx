33e2cb5d85995b4dcfee756706bf5ee349e3a13c
Pedro Escumalha <pedrosimoes93@gmail.com>
2024-05-19 15:48:58 +0200

    Update paginator.tsx

--- frontend/src/components/paginator.tsx
+++ frontend/src/components/paginator.tsx
@@ -1,25 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Pagination, PaginationContent, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from './cn/pagination'
 
-export function Paginator() {
+type PaginatorProps = {
+  numOfPages: number;
+  onPageClick: (page: number) => Promise<void>
+}
+
+
+export function Paginator(props: PaginatorProps) {
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const buildLinks = () => {
+    const links: React.ReactElement[] = []
+
+    for (let i = 0; i < props.numOfPages; i++) {
+      const page = i + 1
+
+      links.push(
+        <PaginationItem>
+          <PaginationLink
+            href="#"
+            onClick={() => {
+              setCurrentPage(page)
+              props.onPageClick(page)
+            }}
+            isActive={page === currentPage}
+          >
+            {page}
+          </PaginationLink>
+        </PaginationItem>
+      )
+    }
+
+    return links
+  }
+
   return (
     <div className="flex justify-end p-4">
       <Pagination>
         <PaginationContent>
           <PaginationItem>
-            <PaginationPrevious href="#" />
+            <PaginationPrevious
+              href="#"
+              onClick={async () => {
+                if (currentPage === 1) {
+                  return
+                }
+
+                setCurrentPage(currentPage - 1)
+                await props.onPageClick(currentPage - 1)
+              }}
+            />
           </PaginationItem>
+          { buildLinks() }
           <PaginationItem>
-            <PaginationLink href="#">1</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">2</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">3</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationNext href="#" />
+            <PaginationNext
+              href="#"
+              onClick={async () => {
+                if (currentPage === props.numOfPages) {
+                  return
+                }
+
+                setCurrentPage(currentPage + 1)
+                await props.onPageClick(currentPage + 1)
+              }}
+            />
           </PaginationItem>
         </PaginationContent>
       </Pagination>
